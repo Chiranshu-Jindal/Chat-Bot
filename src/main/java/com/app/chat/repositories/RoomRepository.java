4c204c976e00abb0645e65a0e5834071f827539a
Chiranshu-Jindal <chiranshujindal03@gmail.com>
2025-06-09 15:09:22 +0530

    Moved backend code into backend/ directory

--- src/main/java/com/app/chat/repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.chat.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.chat.entities.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-	Room findByRoomId(String roomId);  
-}
